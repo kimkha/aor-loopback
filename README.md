48b47b9c4da96226ee2f63a22afc2b193ad7a12e
Nguyễn Kim Kha <nkimkha@gmail.com>
2017-03-09 18:25:09 +0700

    copy from french version

---
+++ README.md
@@ -0,0 +1,29 @@
+# French Translations for Admin-on-rest
+
+French translations for [admin-on-rest](https://github.com/marmelab/admin-on-rest), the frontend framework for building admin applications on top of REST services.
+
+![admin-on-rest demo](http://static.marmelab.com/admin-on-rest.gif)
+
+## Installation
+
+```sh
+npm install --save aor-language-french
+```
+
+## Usage
+
+```js
+import frenchMessages from 'aor-language-french';
+
+const messages = {
+    'fr': frenchMessages,
+};
+
+<Admin locale="fr" messages={messages}>
+  ...
+</Admin>
+```
+
+## License
+
+This translation is licensed under the [MIT Licence](LICENSE), and sponsored by [marmelab](http://marmelab.com).
---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+    aor: {
+        action: {
+            delete: 'Supprimer',
+            show: 'Afficher',
+            list: 'Liste',
+            save: 'Enregistrer',
+            create: 'Créer',
+            edit: 'Éditer',
+            cancel: 'Quitter',
+            refresh: 'Actualiser',
+            add_filter: 'Ajouter un filtre',
+            remove_filter: 'Supprimer ce filtre',
+        },
+        boolean: {
+            true: 'Oui',
+            false: 'Non',
+        },
+        page: {
+            list: 'Liste des %{name}',
+            edit: '%{name} #%{id}',
+            show: '%{name} #%{id}',
+            create: 'Créer %{name}',
+            delete: 'Supprimer %{name} #%{id}',
+            dashboard: 'Tableau de bord',
+        },
+        input: {
+            image: {
+                upload_several: 'Déposez les fichiers à uploader, ou cliquez pour en sélectionner.',
+                upload_single: 'Déposez le fichier à uploader, ou cliquez pour le sélectionner.',
+            },
+        },
+        message: {
+            yes: 'Oui',
+            no: 'Non',
+            are_you_sure: 'Êtes-vous sûr ?',
+            about: 'Au sujet de',
+        },
+        navigation: {
+            page_out_of_boundaries: 'La page %{page} est en dehors des limites',
+            page_out_from_end: 'Fin de la pagination',
+            page_out_from_begin: 'La page doit être supérieure à 1',
+            page_range_info: '%{offsetBegin}-%{offsetEnd} sur %{total}',
+            next: 'Suivant',
+            prev: 'Précédent',
+        },
+        auth: {
+            username: 'Identifiant',
+            password: 'Mot de passe',
+            sign_in: 'Connexion',
+            sign_in_error: 'Échec de l\'authentification, merci de réessayer',
+            logout: 'Déconnexion',
+        },
+        notification: {
+            updated: 'Élément mis à jour',
+            created: 'Élément créé',
+            deleted: 'Élément supprimé',
+            item_doesnt_exist: 'L\'élément n\'existe pas',
+        },
+        validation: {
+            required: 'Ce champ est requis',
+        },
+    },
+};
